d3a276f50b60ad34262dc323c37e76814bfda21b
amandalynn1204 <amandalynn1204@icloud.com>
2023-11-29 21:16:29 -0500

    added search form

--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,22 @@ import "./Weather.css";
 export default function weather() {
   return (
     <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-sm-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="search"
+            />
+          </div>
+          <div className="col-sm-3">
+            <input type="submit" value="Search" className="search-btn" />
+          </div>
+        </div>
+      </form>
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-sm-6 ">
           <h1>Cleveland</h1>
           <p>Sunday 3:00 a.m.</p>
           <p>Clouds</p>
